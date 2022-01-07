4bb5301bcc28087f30bb01f1e5910a666a8eb280
Olivia Dang <olivia@alembic.com.au>
2022-01-07 14:25:12 +1100

    create html statement

--- src/statement.test.ts
+++ src/statement.test.ts
@@ -1,17 +1,18 @@
-import { statement } from "./statement";
+import { htmlStatement } from "./statement";
 import plays from "../mock-data/plays.json";
 import invoices from "../mock-data/invoices.json";
 
 describe("Function Statement", () => {
   it("Prints BigCo", () => {
-    expect(statement(invoices[0], plays)).toEqual(
-      `Statement for BigCo
- Hamlet: $650.00 (55 seats)
- As You Like It: $580.00 (35 seats)
- Othello: $500.00 (40 seats)
-Amount owed is $1,730.00
-You earned 47 credits
-`
+    expect(htmlStatement(invoices[0], plays)).toEqual(
+      `<h1>Statement for ${invoices[0].customer}</h1>
+<table>
+<tr><th>play</th><th>seats</th><th>cost</th></tr> <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+ <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+ <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+</table>
+<p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>47</em> credits</p>`
     );
   });
 });
--- src/statement.ts
+++ src/statement.ts
@@ -1,22 +1,22 @@
 import { IInvoice, IPerformance, IPlays } from "./type-helper";
 import { createStatementData } from "./create-statement-data";
 
-export function statement(invoice: IInvoice, plays: IPlays) {
-  return renderPlainText(createStatementData(invoice, plays));
+export function htmlStatement(invoice: IInvoice, plays: IPlays) {
+  return renderhtml(createStatementData(invoice, plays));
 }
 
-function renderPlainText(data: any) {
-  let result = `Statement for ${data.customer}\n`;
-
+function renderhtml(data: any) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
   for (let perf of data.performances) {
-    // print line for this order
-    result += ` ${perf.play.name}: ${toUsd(perf.amount)} (${
-      perf.audience
-    } seats)\n`;
+    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${toUsd(perf.amount)}</td></tr>\n`;
   }
-
-  result += `Amount owed is ${toUsd(data.totalAmount)}\n`;
-  result += `You earned ${data.totalVolumeCredits} credits\n`;
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${toUsd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>`;
+  console.log(result);
   return result;
 
   function toUsd(aNumber: number) {
