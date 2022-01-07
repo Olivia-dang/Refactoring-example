693fcfa3f3b224021f6ba40d406f87193e5d7f86
Olivia Dang <olivia@alembic.com.au>
2022-01-07 13:56:50 +1100

    ref helper functions

--- src/refactoring.ts
+++ src/refactoring.ts
@@ -4,47 +4,25 @@ export function statement(invoice: IInvoice, plays: IPlays) {
   const statementData: any = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+  console.log(statementData.performances);
   return renderPlainText(statementData, invoice, plays);
+
+  function totalAmount(data) {
+    return data.performances.reduce((acc, perf) => acc + perf.amount, 0);
+  }
+
+  function totalVolumeCredits(data) {
+    return data.performances.reduce((acc, perf) => acc + perf.volumeCredits, 0);
+  }
 
   function enrichPerformance(aPerformance: IPerformance) {
     // shallow copy object => immutable origin
     const result: any = Object.assign({}, aPerformance);
     result.play = playFor(result);
-    return result;
-  }
-
-  function playFor(perf: IPerformance) {
-    return plays[perf.playID];
-  }
-}
-
-function renderPlainText(data: any, invoice: IInvoice, plays: IPlays) {
-  let result = `Statement for ${data.customer}\n`;
-
-  for (let perf of data.performances) {
-    // print line for this order
-    result += ` ${perf.play.name}: ${toUsd(
-      amountFor(perf.audience, perf.play.type)
-    )} (${perf.audience} seats)\n`;
-  }
-
-  result += `Amount owed is ${toUsd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
-  return result;
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += amountFor(perf.audience, perf.play.type);
-    }
-    return result;
-  }
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
-    }
+    result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -57,33 +35,53 @@ function renderPlainText(data: any, invoice: IInvoice, plays: IPlays) {
       result += Math.floor(aPerformance.audience / 5);
     return result;
   }
+
+  function playFor(perf: IPerformance) {
+    return plays[perf.playID];
+  }
+
+  function amountFor(aPerformance: any) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
+}
+
+function renderPlainText(data: any, invoice: IInvoice, plays: IPlays) {
+  let result = `Statement for ${data.customer}\n`;
+
+  for (let perf of data.performances) {
+    // print line for this order
+    result += ` ${perf.play.name}: ${toUsd(perf.amount)} (${
+      perf.audience
+    } seats)\n`;
+  }
+
+  result += `Amount owed is ${toUsd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
+  return result;
+
   function toUsd(aNumber: number) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
-  }
-
-  function amountFor(numberOfAudience: number, playType: string) {
-    let result = 0;
-    switch (playType) {
-      case "tragedy":
-        result = 40000;
-        if (numberOfAudience > 30) {
-          result += 1000 * (numberOfAudience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (numberOfAudience > 20) {
-          result += 10000 + 500 * (numberOfAudience - 20);
-        }
-        result += 300 * numberOfAudience;
-        break;
-      default:
-        throw new Error(`unknown type: ${playType}`);
-    }
-    return result;
   }
 }
